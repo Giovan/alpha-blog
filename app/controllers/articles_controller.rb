712af38e6235d9ad50e8dcb3671bb1b6f05fe99f
Giovanni <giovanniyatze@gmail.com>
2019-05-24 03:54:01 -0500

    Add Refactoring for article search by ID. And destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  #before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -25,7 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -35,10 +33,17 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article.destroy
+    flash[:another] = "Another was successfully deleted"
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:title, :description)
